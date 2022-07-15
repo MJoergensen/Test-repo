9571d4447572fbe551be62522ea2c50468d0cac1
Martin Jørgensen <M.Joergensen1988@gmail.com>
2022-07-15 09:35:12 +0200

    Initial Commit

---
+++ main.bat
@@ -0,0 +1,8 @@
+@echo off
+
+START Notepad.exe "%UserProfile%\Documents\GitHub\Test\Test-repo\Apple_pie.txt"
+pause
+
+START Notepad.exe "%UserProfile%\Documents\GitHub\Test\Test-repo\Pear_pie.txt"
+
+pause
